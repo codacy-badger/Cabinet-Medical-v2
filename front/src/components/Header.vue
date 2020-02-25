a97f509bbfa5aef6fce8b3567f7c8e3af69f9077
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-25 16:14:46 +0100

    add content for the part template and style of the component Header.vue

---
+++ front/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="overflow-hidden">
+    <div class="center">
+      <v-toolbar-title>Nom de l'entreprise</v-toolbar-title> 
+     <v-btn>
+      <router-link to="/"><strong>Redirection 1</strong></router-link>
+    </v-btn>
+    <v-btn>
+ <router-link to="/"><strong>Redirection 2</strong></router-link> 
+    </v-btn>
+    <v-btn>
+ <router-link to="/"><strong>Redirection 3</strong></router-link>
+    </v-btn>
+
+    <v-btn>
+ <router-link to="/"><strong>Redirection 4</strong></router-link> 
+    </v-btn>
+    <v-btn>
+ <router-link to="/"><strong>Redirection 5</strong></router-link>  
+   </v-btn>
+  </div>
+  </v-card>
+</template>
+
+
+
+<style>
+.center{
+    text-align: center;
+}
+</style>
