6fdbea1c5da6473d70222bbac06b48830b4b872d
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-25 21:20:59 +0100

    add the part for the second doctor in the component ContentHome.vue

--- front/src/components/ContentHome.vue
+++ front/src/components/ContentHome.vue
@@ -7,6 +7,13 @@
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum labore, distinctio culpa sapiente et veniam vel repellat, quibusdam perspiciatis accusamus doloremque ex, explicabo praesentium vitae facere voluptate! Porro, repellat commodi! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt sint quis similique quae illum modi qui commodi aspernatur magnam corrupti mollitia recusandae, officiis voluptatem necessitatibus sapiente excepturi iusto quos autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui, corrupti impedit, fugiat iure quaerat consequatur quam assumenda minima quibusdam rem quae ut quisquam facilis, ab a exercitationem dicta pariatur.
             </p>     
             </v-col>
+            <br>
+              <v-col>                 
+            <h1>Nom du médecin n°2</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum labore, distinctio culpa sapiente et veniam vel repellat, quibusdam perspiciatis accusamus doloremque ex, explicabo praesentium vitae facere voluptate! Porro, repellat commodi! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt sint quis similique quae illum modi qui commodi aspernatur magnam corrupti mollitia recusandae, officiis voluptatem necessitatibus sapiente excepturi iusto quos autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui, corrupti impedit, fugiat iure quaerat consequatur quam assumenda minima quibusdam rem quae ut quisquam facilis, ab a exercitationem dicta pariatur.
+            </p>     
+            </v-col>
     </v-container>
   </div>
 </template>
