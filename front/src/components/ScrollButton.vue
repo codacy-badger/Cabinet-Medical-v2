bba54656ce7ce60ef1410a111f47ab93472d0486
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-25 20:52:00 +0100

    add content in the part template and script in the component ScrollButton.vue

---
+++ front/src/components/ScrollButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-btn
+    v-scroll="onScroll"
+    v-show="fab"
+    fab
+    dark
+    fixed
+    bottom
+    right
+    @click="toTop"
+    color="primary"
+    >
+            <v-icon class="fas fa-arrow-up"></v-icon>
+          </v-btn>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fab: false
+    };
+  },
+  methods: {
+    onScroll(e) {
+      if (typeof window === "undefined") return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    }
+  }
+};
+</script>
