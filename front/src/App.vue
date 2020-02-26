7f521bcdbe01ec8b674ea4e9b90e2edaf213b6d0
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-26 20:01:00 +0100

    import the component Footer in App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,18 +3,21 @@
      <Header/>
     <router-view />
     <ScrollButton/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
 import ScrollButton from './components/ScrollButton'
+import Footer from './components/Footer'
 
 export default {
   name: "App",
   components: {
     Header,
-    ScrollButton
+    ScrollButton,
+    Footer
   }
 };
 </script>
