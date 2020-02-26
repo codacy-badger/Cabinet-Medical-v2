27996cb08946d90b143e463c7409e7bb6f097dae
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-26 20:18:59 +0100

    rename the class color to colorContent in the component ContentHome

--- front/src/components/ContentHome.vue
+++ front/src/components/ContentHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="color">
+  <div class="colorContent">
     <h1 class="center">Présentation du cabinet</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam totam et, tenetur aspernatur unde sunt rerum at, ipsam excepturi fuga consectetur ex? Dolores perspiciatis eius quisquam cumque repellendus quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum corrupti minima eaque autem dignissimos provident expedita quaerat libero tempora, minus commodi saepe nostrum voluptate. Rem ducimus debitis inventore iste ipsum? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, saepe sequi cumque sunt rerum laboriosam vitae omnis, ipsa ipsum libero optio pariatur ab. Animi, officiis cum laboriosam autem commodi officia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta iste dolorem sequi accusamus expedita molestiae error debitis, non libero deleniti fuga illum corporis ducimus et aperiam saepe provident recusandae?
     </p>
@@ -17,7 +17,7 @@
 .center{
   text-align: center;
 }
-.color{
+.colorContent{
   background-color: #64b2cd
 }
 </style>
