ab6dafbc88fb0ef92560cfb8dc331f0103bb4d4b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-26 20:16:43 +0100

    add a backgroundcolor in the component Footer

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,10 +1,17 @@
 <template>
   <v-footer padless>
     <v-col
-      class="text-center"
+      class="FooterColor"
       cols="12"
+      
     >
       {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
     </v-col>
   </v-footer>
 </template>
+
+<style>
+.FooterColor{
+    background-color: #64b2cd
+}
+</style>
