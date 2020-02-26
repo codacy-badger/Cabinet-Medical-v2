47b78480226cdd197e4c88a8c8dee9d762861853
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-26 15:00:09 +0100

    add the google map in the component ContentHome.vue

--- front/src/components/ContentHome.vue
+++ front/src/components/ContentHome.vue
@@ -3,6 +3,13 @@
     <h1 class="center">Présentation du cabinet</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam totam et, tenetur aspernatur unde sunt rerum at, ipsam excepturi fuga consectetur ex? Dolores perspiciatis eius quisquam cumque repellendus quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum corrupti minima eaque autem dignissimos provident expedita quaerat libero tempora, minus commodi saepe nostrum voluptate. Rem ducimus debitis inventore iste ipsum? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, saepe sequi cumque sunt rerum laboriosam vitae omnis, ipsa ipsum libero optio pariatur ab. Animi, officiis cum laboriosam autem commodi officia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta iste dolorem sequi accusamus expedita molestiae error debitis, non libero deleniti fuga illum corporis ducimus et aperiam saepe provident recusandae?
     </p>
+     <v-container>
+    <v-row class="ml-12">
+      <v-col cols="12">
+          <div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=50.643039, 3.056496&amp;q=37%20rue%20voltaire%2059000%20Lille+(Cabinet%20m%C3%A9dical)&amp;ie=UTF8&amp;t=h&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">find my coordinates</a></iframe></div><br />
+      </v-col>
+    </v-row>
+  </v-container>
   </div>
 </template>
 
