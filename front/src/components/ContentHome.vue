27f0eea4afd07b2c8aa00c45a363e9db8cb794ae
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-26 14:43:10 +0100

    add a h1 with some lorem ipsum in it in the component ContentHome.vue

---
+++ front/src/components/ContentHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="center">Présentation du cabinet</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam totam et, tenetur aspernatur unde sunt rerum at, ipsam excepturi fuga consectetur ex? Dolores perspiciatis eius quisquam cumque repellendus quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum corrupti minima eaque autem dignissimos provident expedita quaerat libero tempora, minus commodi saepe nostrum voluptate. Rem ducimus debitis inventore iste ipsum? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, saepe sequi cumque sunt rerum laboriosam vitae omnis, ipsa ipsum libero optio pariatur ab. Animi, officiis cum laboriosam autem commodi officia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta iste dolorem sequi accusamus expedita molestiae error debitis, non libero deleniti fuga illum corporis ducimus et aperiam saepe provident recusandae?
+    </p>
+  </div>
+</template>
+
+<style>
+.center{
+  text-align: center;
+}
+</style>
