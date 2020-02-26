5d4554350d6c519f65845d3fae3f8d58b1b7936e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-26 14:27:26 +0100

    import the component Contentabout in views/About.vue

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,4 +1,14 @@
 <template>
   <div class="about">
+    <ContentAbout/>
   </div>
 </template>
+
+<script>
+import ContentAbout from '../components/ContentAbout'
+export default {
+  components:{
+    ContentAbout,
+  }
+}
+</script>
